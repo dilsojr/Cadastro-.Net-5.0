f94820d5d6ef734b8ccca9de4672e9760d67bcb7
dilsojr <51032855+dilsojr@users.noreply.github.com>
2021-11-23 22:41:57 -0300

    Criação do ContextFactory e Ajustes das views

---
+++ source/Site.Dados/Contexto/MeuDbContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Site.Dados.Contexto
+{
+    //https://docs.microsoft.com/pt-br/ef/core/cli/dbcontext-creation?tabs=vs
+    public class MeuDbContextFactory : IDesignTimeDbContextFactory<MeuDbContext>
+    {
+        public MeuDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<MeuDbContext>();
+            optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=ProjetoCadastro;uid=sa;pwd=sa;MultipleActiveResultSets=true");
+
+            return new MeuDbContext(optionsBuilder.Options);
+        }
+    }
+}
